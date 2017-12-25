04a551640178fe27c5b4e2f0d4401f0ba71608f0
jooseppi-luna <jooseppi_luna@student.uml.edu>
2017-12-24 20:14:50 -0500

    did a lot

---
+++ header.js
@@ -0,0 +1,12 @@
+document.write('\
+<h1>Jooseppi Luna</h1>\
+<h2>Aspiring sotware engineer with a passion for efficiency and simplicity</h2>\
+\
+<h3>\
+<a href=index.html>About</a>\
+<a href=experience.html>Work Experience</a>\
+<a href=projects.html>Projects</a>\
+<a href=contact.html>Contact/Resume</a>\
+</h3>\
+\
+')
