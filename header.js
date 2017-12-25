1a862a87b50f06e64dd92b0766a9402d5f16de1b
jooseppi-luna <jooseppi_luna@student.uml.edu>
2017-12-25 17:25:28 -0500

    about now links to main site

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ document.write('\
 <h2>Aspiring sotware engineer with a passion for efficiency and simplicity</h2>\
 \
 <ul class="nav">\
-<li><a href=index.html>About</a></li>\
+<li><a href=jooseppiluna.com>About</a></li>\
 <li><a href=experience.html>Experience</a></li>\
 <li><a href=projects.html>Projects</a></li>\
 <li><a href=contact.html>Contact</a></li>\
