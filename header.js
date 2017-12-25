f68e14943b5d24f460ab419a813deb4a3751665b
jooseppi-luna <jooseppi_luna@student.uml.edu>
2017-12-24 22:38:40 -0500

    header pretty much finished

--- header.js
+++ header.js
@@ -2,11 +2,12 @@ document.write('\
 <h1>Jooseppi Luna</h1>\
 <h2>Aspiring sotware engineer with a passion for efficiency and simplicity</h2>\
 \
-<h3>\
-<a href=index.html>About</a>\
-<a href=experience.html>Work Experience</a>\
-<a href=projects.html>Projects</a>\
-<a href=contact.html>Contact/Resume</a>\
+<ul class="nav">\
+<li><a href=index.html>About</a></li>\
+<li><a href=experience.html>Experience</a></li>\
+<li><a href=projects.html>Projects</a></li>\
+<li><a href=contact.html>Contact</a></li>\
+</ul>\
 </h3>\
 \
 ')
