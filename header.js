fd05b9da9db1834938958305231933d25fab9252
jooseppi-luna <jooseppi_luna@student.uml.edu>
2017-12-25 02:29:52 -0500

    finished main, next need to throw in picture

--- header.js
+++ header.js
@@ -7,6 +7,7 @@ document.write('\
 <li><a href=experience.html>Experience</a></li>\
 <li><a href=projects.html>Projects</a></li>\
 <li><a href=contact.html>Contact</a></li>\
+<li><a href=Jooseppi_Luna_Resume.pdf>Resume</a></li>\
 </ul>\
 </h3>\
 \
