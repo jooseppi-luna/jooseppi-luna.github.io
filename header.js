6b8c38ccdc1eb606b50e1cc85c02541593961d7a
jooseppi-luna <jooseppi_luna@student.uml.edu>
2017-12-25 17:51:42 -0500

    switched linnks back to index.html because of errors

--- header.js
+++ header.js
@@ -1,14 +1,13 @@
 document.write('\
-<h1><a href=jooseppiluna.com>Jooseppi Luna</a></h1>\
+<h1><a href=index.html>Jooseppi Luna</a></h1>\
 <h2>Aspiring sotware engineer with a passion for efficiency and simplicity</h2>\
 \
 <ul class="nav">\
-<li><a href=jooseppiluna.com>About</a></li>\
+<li><a href=index.html>About</a></li>\
 <li><a href=experience.html>Experience</a></li>\
 <li><a href=projects.html>Projects</a></li>\
 <li><a href=contact.html>Contact</a></li>\
 <li><a href=Jooseppi_Luna_Resume.pdf>Resume</a></li>\
 </ul>\
-</h3>\
 \
 ')
